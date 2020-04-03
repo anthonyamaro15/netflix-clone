ce5ec28dfe16190438c54150bcdc09b2fffde749
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-04-02 19:25:44 -0700

    added background color when scrolling down successfully

--- movie-app/src/components/Navbar.js
+++ movie-app/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
@@ -6,12 +6,32 @@ import NavForm from "./NavForm";
 
 const Navbar = () => {
   const [showForm, setShowForm] = useState(false);
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY <= 500) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+
+      if (window.scrollY === 0) {
+        setScrolling(false);
+      }
+    });
+  });
 
   const toggleForm = () => {
     setShowForm(!showForm);
   };
+
+  const isUserScrolling = scrolling
+    ? "navbar-container black"
+    : "navbar-container";
+
   return (
-    <div className="navbar-container">
+    <div className={isUserScrolling}>
       <header>
         <div className="logo">
           <h2>Movie App</h2>
