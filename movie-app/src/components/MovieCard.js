46338e1f7c132483e342e1e3f19c376bd091592e
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-04-02 19:06:54 -0700

    added single movie card boiler plate

--- movie-app/src/components/MovieCard.js
+++ movie-app/src/components/MovieCard.js
@@ -1,12 +1,22 @@
 import React from "react";
 import img from "../img/bg.jpg";
 // import MovieContent from './MovieContent';
+import { MdAddCircleOutline } from "react-icons/md";
 
 const MovieCard = () => {
   return (
     <div className="single-movie-container">
       <div className="img-container">
         <img src={img} alt="image description" />
+        <div className="hover-info">
+          <h3>all american</h3>
+          <p>rating 7.2</p>
+          <div className="add-icon">
+            <div>
+              <MdAddCircleOutline />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
