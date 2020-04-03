652d949e14e40f14100264a1d7e52d92eed4cdc6
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-04-02 23:35:20 -0700

    made some changes to background header image

--- movie-app/src/components/Header.js
+++ movie-app/src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import la from "../img/people.jpg";
 
 const Header = () => {
   return (
     <div className="header-container">
+      <img src={la} />
       <div className="movie-info">
         <h1>all american</h1>
         <p className="movie-description">
