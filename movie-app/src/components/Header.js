2593e9f8d3a37fdd276b4117846e85bf5bd5a357
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-04-05 13:53:43 -0700

    updated the comment instructions

--- movie-app/src/components/Header.js
+++ movie-app/src/components/Header.js
@@ -1,43 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useRouteMatch } from "react-router-dom";
 import la from "../img/people.jpg";
-// import { FaCommentDollar } from "react-icons/fa";
 
-const Header = ({ popular, loading }) => {
-  //   const [values] = header[0];
-  //   console.log(header[0]);
-  //   const { name, title, overview, backdrop_path } = header[0];
-  //   const obj = header[0];
+const Header = ({ popular }) => {
+  const { url } = useRouteMatch();
 
-  console.log("data here ", popular[0]);
-  //   const randomMovie = () => {
-  //     let random = Math.floor(Math.random() * popular.length);
-  //     return popular[random];
-  //   };
-  //   console.log(randomMovie());
-  //   console.log(loading);
+  //   console.log("data here ", popular[0]);
 
   return (
     <div className="header-container">
       <img
         src={
-          popular[0] === undefined
+          popular[18] === undefined
             ? la
-            : `https://image.tmdb.org/t/p/original${popular[0].backdrop_path}`
+            : `https://image.tmdb.org/t/p/original${popular[18].backdrop_path}`
         }
         alt="header of the application"
       />
       <div className="movie-info">
         <h1>
-          {popular[0] === undefined
+          {popular[18] === undefined
             ? "all american"
-            : popular[0].original_title || popular[0].original_name}
+            : popular[18].original_title || popular[18].original_name}
         </h1>
         <p className="movie-description">
-          {popular[0] === undefined ? "loading..." : popular[0].overview}
+          {popular[18] === undefined ? "loading..." : popular[18].overview}
         </p>
         <div className="more-info">
-          <Link to="/moreinfo">more info</Link>
+          <Link
+            to={`${url}/${
+              popular[18] === undefined ? "/moreinfo" : popular[18].id
+            }`}
+          >
+            more info
+          </Link>
         </div>
       </div>
     </div>
