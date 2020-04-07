026aa658faeee6b75abb20dd400723cc479741f4
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-04-06 18:02:59 -0700

    breaking reducer files into its own file

---
+++ movie-app/src/reducers/tvPopularReducer.js
@@ -0,0 +1,28 @@
+export const tvPopularReducer = (state = initialValue, action) => {
+  switch (action.type) {
+    case "FETCHING_TV_DATA":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "GETTING_TV_DATA":
+      return {
+        ...state,
+        tvPopular: [...state.tvPopular, ...action.payload],
+        loading: false,
+      };
+    case "NEXT_PAGE_POPULAR":
+      return {
+        ...state,
+        tvPopularPage: state.tvPopularPage + 1,
+      };
+    case "ERROR_TV":
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
