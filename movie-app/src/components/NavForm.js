f0f9feb552849d17ac056b94fd6af0b442d97d55
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-04-06 18:25:24 -0700

    removed withFormik and added Formik successfully

--- movie-app/src/components/NavForm.js
+++ movie-app/src/components/NavForm.js
@@ -1,52 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useHistory } from "react-router-dom";
-import { withFormik, Formik, Form, Field } from "formik";
+import { Formik, Form, Field } from "formik";
 import { useDispatch } from "react-redux";
 import * as yup from "yup";
-
-// const NavForm = (props) => {
-//   const { status, setShowForm, touched, errors } = props;
-//   const dispatch = useDispatch();
-//   const history = useHistory();
-
-//   useEffect(() => {
-//     status && dispatch({ type: "SEARCH_MOVIE", payload: status.search });
-//     status && setShowForm(false);
-//   }, [status, dispatch, setShowForm]);
-
-//   useEffect(() => {
-//     status && history.push("/results");
-//   }, [status, history]);
-
-//   return (
-//     <Form className="form">
-//       <label htmlFor="search">
-//         <Field
-//           type="text"
-//           name="search"
-//           id="search"
-//           placeholder="Movie title"
-//         />
-//       </label>
-//       {errors.search && touched.search && (
-//         <p className="error">{errors.search}</p>
-//       )}
-//     </Form>
-//   );
-// };
-
-// export default withFormik({
-//   mapPropsToValues: () => ({
-//     search: "",
-//   }),
-//   validationSchema: yup.object().shape({
-//     search: yup.string().required("please enter movie title"),
-//   }),
-//   handleSubmit: (values, { resetForm, setStatus }) => {
-//     setStatus(values);
-//     resetForm();
-//   },
-// })(NavForm);
 
 const validationSchema = yup.object().shape({
   search: yup.string().required("please enter movie title"),
@@ -55,6 +11,7 @@ const validationSchema = yup.object().shape({
 const NavForm = ({ setShowForm }) => {
   const dispatch = useDispatch();
   const history = useHistory();
+
   return (
     <Formik
       initialValues={{ search: "" }}
