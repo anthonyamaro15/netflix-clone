1ff95147369596e946206d1a17a9598c15eca7c5
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-04-18 22:41:19 -0700

    added comments on what needs to be done

--- movie-app/src/components/homePage/Home.js
+++ movie-app/src/components/homePage/Home.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 // add home page with sign up and login nav
+// need to get the login working as well
+// allow only users to see the movies in the page
+// fix router for movies
+// fix bug in the navbar search
+// add media queries
+// find a way to make page load faster
 
 const Home = () => {
   return (
