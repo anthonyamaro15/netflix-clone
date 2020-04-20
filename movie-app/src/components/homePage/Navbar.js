946affe9f134395571fc34565995f4940a440839
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-04-19 22:56:35 -0700

    fix bug

---
+++ movie-app/src/components/homePage/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Navbar;
