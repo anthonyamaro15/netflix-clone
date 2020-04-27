6fe6d245b5b5fc94f4ac23c861f4e91637425ed9
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-04-26 20:59:40 -0700

    added validation for signup/login forms

--- movie-app/src/components/homePage/Signup.js
+++ movie-app/src/components/homePage/Signup.js
@@ -1,6 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Formik, Field, Form } from "formik";
+import * as yup from "yup";
+
+const validationSchema = yup.object().shape({
+  name: yup.string().required("Please enter name"),
+  email: yup
+    .string()
+    .lowercase()
+    .email("invalid email")
+    .required("Please enter email"),
+  password: yup
+    .string()
+    .min(4, "Too short")
+    .max(15, "Too long")
+    .required("Please enter password"),
+  confirm: yup
+    .string()
+    .oneOf([yup.ref("password")], "Password does not  match")
+    .required("Please confirm password"),
+});
 
 const Signup = () => {
   return (
@@ -8,17 +27,20 @@ const Signup = () => {
       <div className="Signup-form">
         <h2>Sign Up</h2>
         <Formik
-          initialValues={{ name: "", email: "", password: "" }}
+          initialValues={{ name: "", email: "", password: "", confirm: "" }}
+          validationSchema={validationSchema}
           onSubmit={(values, { resetForm }) => {
             console.log(values);
             resetForm();
           }}
         >
-          {() => (
+          {({ errors, touched }) => (
             <Form>
               <label htmlFor="name">
                 <Field type="text" name="name" id="name" placeholder="name" />
-                <p className="error-message">your name must be valid</p>
+                {errors && touched && (
+                  <p className="error-message">{errors.name}</p>
+                )}
               </label>
               <label htmlFor="email">
                 <Field
@@ -27,7 +49,9 @@ const Signup = () => {
                   id="email"
                   placeholder="email"
                 />
-                <p className="error-message">your name must be valid</p>
+                {errors && touched && (
+                  <p className="error-message">{errors.email}</p>
+                )}
               </label>
               <label htmlFor="password">
                 <Field
@@ -36,7 +60,20 @@ const Signup = () => {
                   id="password"
                   placeholder="password"
                 />
-                <p className="error-message">your name must be valid</p>
+                {errors && touched && (
+                  <p className="error-message">{errors.password}</p>
+                )}
+              </label>
+              <label htmlFor="confirm">
+                <Field
+                  type="password"
+                  name="confirm"
+                  id="confirm"
+                  placeholder="confirm password"
+                />
+                {errors && touched && (
+                  <p className="error-message">{errors.confirm}</p>
+                )}
               </label>
               <button type="submit">Sign Up</button>
             </Form>
