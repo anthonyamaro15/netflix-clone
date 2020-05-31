264badd2472afafb00bee5e25b6846931b03fe88
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-05-30 18:23:41 -0700

    done

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import { rootReducer } from "./reducers/index";
 
 import "./index.scss";
 import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
@@ -27,3 +28,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
