b0aa8853524156d846f52679d4359effb0a96eed
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-08-12 17:24:29 -0700

    deleted unnecessary code

--- src/components/MainApp.js
+++ src/components/MainApp.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import { Route } from "react-router-dom";
