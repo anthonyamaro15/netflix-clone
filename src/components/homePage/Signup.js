0e8135b9481c38333d1291adad979d11151df011
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-08-12 17:18:33 -0700

    removed formik for react-hook-forms, updated dependencies

--- src/components/homePage/Signup.js
+++ src/components/homePage/Signup.js
@@ -1,36 +1,43 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
-import { Formik, Field, Form } from "formik";
-import * as yup from "yup";
+import { useForm } from "react-hook-form";
 import { axiosWithAuthDB } from "../../utils/axiosWithAuth";
 import Navbar from "./Navbar";
-
-const validationSchema = yup.object().shape({
-  username: yup.string().required("Please enter username"),
-  email: yup
-    .string()
-    .lowercase()
-    .email("invalid email")
-    .required("Please enter email"),
-  password: yup
-    .string()
-    .min(4, "Too short")
-    .max(15, "Too long")
-    .required("Please enter password"),
-  confirm: yup
-    .string()
-    .oneOf([yup.ref("password")], "Password does not  match")
-    .required("Please confirm password"),
-});
 
 const Signup = () => {
   const history = useHistory();
   const [loading, setLoading] = useState(false);
   const [errorr, setError] = useState("");
   const [togglePass, setTogglePass] = useState(false);
+  const { register, handleSubmit, errors, reset } = useForm();
 
   const toggle = () => {
     setTogglePass(!togglePass);
+  };
+
+  const onSubmit = (values) => {
+    const { username, email, password } = values;
+    const storeValues = {
+      email,
+      username,
+      password,
+    };
+    setLoading(true);
+    axiosWithAuthDB()
+      .post("/api/auth/register", storeValues)
+      .then((res) => {
+        history.push("/login");
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err.response.data.message);
+        setError(err.response.data.message);
+        setLoading(false);
+      });
+
+    if (loading) {
+      reset();
+    }
   };
   return (
     <div>
@@ -38,102 +45,68 @@ const Signup = () => {
       <div className="Signup">
         <div className="Signup-form">
           <h2>Sign Up</h2>
-          <Formik
-            initialValues={{
-              username: "",
-              email: "",
-              password: "",
-              confirm: "",
-            }}
-            validationSchema={validationSchema}
-            onSubmit={(values, { resetForm }) => {
-              const { username, email, password } = values;
-              const storeValues = {
-                email,
-                username,
-                password,
-              };
-              setLoading(true);
-              axiosWithAuthDB()
-                .post("/api/auth/register", storeValues)
-                .then((res) => {
-                  history.push("/login");
-                  setLoading(false);
-                })
-                .catch((err) => {
-                  console.log(err.response.data.message);
-                  setError(err.response.data.message);
-                  setLoading(false);
-                });
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="username">
+              <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="username"
+                ref={register({ required: true })}
+              />
+              <p className="error-message">
+                {errors.username && "Require Field"}
+              </p>
+            </label>
+            <label htmlFor="email">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="email"
+                ref={register({ required: true })}
+              />
+              <p className="error-message">{errors.email && "Require Field"}</p>
+            </label>
+            <label htmlFor="password">
+              <input
+                type={!togglePass ? "password" : "text"}
+                name="password"
+                id="password"
+                placeholder="password"
+                ref={register({ required: true })}
+              />
+              <span className="togglePassword" onClick={toggle}>
+                {!togglePass ? "show" : "hide"}
+              </span>
+              <p className="error-message">
+                {errors.password && "Require Field"}
+              </p>
+            </label>
+            <label htmlFor="confirm">
+              <input
+                type={!togglePass ? "password" : "text"}
+                name="confirm"
+                id="confirm"
+                placeholder="confirm password"
+                ref={register({ required: true })}
+              />
+              <span className="togglePassword" onClick={toggle}>
+                {!togglePass ? "show" : "hide"}
+              </span>
+              <p className="error-message">
+                {errors.confirm && "Require Field"}
+              </p>
+            </label>
 
-              if (loading) {
-                resetForm();
-              }
-            }}
-          >
-            {({ errors, touched }) => (
-              <Form>
-                <label htmlFor="username">
-                  <Field
-                    type="text"
-                    name="username"
-                    id="username"
-                    placeholder="username"
-                  />
-                  {errors && touched && (
-                    <p className="error-message">{errors.name}</p>
-                  )}
-                </label>
-                <label htmlFor="email">
-                  <Field
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="email"
-                  />
-                  {errors && touched && errorr && (
-                    <p className="error-message">{errors.email || errorr}</p>
-                  )}
-                </label>
-                <label htmlFor="password">
-                  <Field
-                    type={!togglePass ? "password" : "text"}
-                    name="password"
-                    id="password"
-                    placeholder="password"
-                  />
-                  <span className="togglePassword" onClick={toggle}>
-                    {!togglePass ? "show" : "hide"}
-                  </span>
-                  {errors && touched && (
-                    <p className="error-message">{errors.password}</p>
-                  )}
-                </label>
-                <label htmlFor="confirm">
-                  <Field
-                    type={!togglePass ? "password" : "text"}
-                    name="confirm"
-                    id="confirm"
-                    placeholder="confirm password"
-                  />
-                  <span className="togglePassword" onClick={toggle}>
-                    {!togglePass ? "show" : "hide"}
-                  </span>
-                  {errors && touched && (
-                    <p className="error-message">{errors.confirm}</p>
-                  )}
-                </label>
-
-                <button
-                  type="submit"
-                  disabled={loading}
-                  className={loading ? "submitting btn" : "btn"}
-                >
-                  {loading ? "Creating account..." : "Sign Up"}
-                </button>
-              </Form>
-            )}
-          </Formik>
+            <button
+              type="submit"
+              disabled={loading}
+              className={loading ? "submitting btn" : "btn"}
+            >
+              {loading ? "Creating account..." : "Sign Up"}
+            </button>
+          </form>
           <p className="already-member">
             Already a member? <Link to="/login">Log In</Link>
           </p>
