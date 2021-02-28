b983cf0ab1515784ab43d45f0fa11ca9ff5fc2e6
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2021-02-28 13:17:48 -0800

    refacor(redux) add redux actions
    created new redux files and now using variables in a single place for all the acitons instead of hard coding them

--- src/reducers/favoriteListReducer.js
+++ src/reducers/favoriteListReducer.js
@@ -1,8 +1,9 @@
 import { initialValue } from "./initialValues";
+import { GET_FAVORITE_LIST } from '../redux/actions';
 
 export const favoriteListReducer = (state = initialValue, action) => {
   switch (action.type) {
-    case "GET_FAVORITE_LIST":
+    case GET_FAVORITE_LIST:
       return {
         ...state,
         favoriteList: action.payload,
--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -1,25 +1,31 @@
 import { initialValue } from "./initialValues";
+import { 
+   SEARCH_MOVIE,
+   FETCHING_SEARCH,
+   GETTING_SEARCH_VALUES,
+   ERROR_SEARCH
+} from '../redux/actions';
 
 export const searchReducer = (state = initialValue, action) => {
   switch (action.type) {
-    case "SEARCH_MOVIE":
+    case SEARCH_MOVIE:
       return {
         ...state,
         movieSearch: action.payload,
       };
-    case "FETCHING_SEARCH":
+    case FETCHING_SEARCH:
       return {
         ...state,
         loading: true,
       };
-    case "GETTING_SEARCH_VALUES":
+    case GETTING_SEARCH_VALUES:
       return {
         ...state,
         movieSearchResponse: action.payload,
         loading: false,
       };
 
-    case "ERROR_SEARCH":
+    case ERROR_SEARCH:
       return {
         ...state,
         error: action.payload,
