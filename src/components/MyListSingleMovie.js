16e4f00971223a3204c2b36e77b615a472952f7a
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2021-02-28 15:50:03 -0800

    refactor(nav-form): fix form bug
    fixed search movie form, added new link because the old way of doing it wasnt working any more

--- src/components/MyListSingleMovie.js
+++ src/components/MyListSingleMovie.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import la from "../img/people.jpg";
+import backupImg from "../img/people.jpg";
 
 const MyListSingleMovie = ({ movie }) => {
   const [movies, setMovies] = useState(movie);
@@ -11,21 +11,14 @@ const MyListSingleMovie = ({ movie }) => {
     setMovies(movie);
   }, [movies, movie]);
 
+  const movieImg = `https://image.tmdb.org/t/p/original${poster_path || backdrop_path}` || backupImg;
+
   return (
     <div className="single-movie-container">
       <Link to={`/acc/mylist/${movie_id}`}>
         <div className="img-container">
-          <img
-            src={
-              poster_path || backdrop_path
-                ? `https://image.tmdb.org/t/p/original${
-                    poster_path || backdrop_path
-                  }`
-                : la
-            }
-            alt={title}
-          />
-          <div className={joined ? "hover-info" : "hover-info"}>
+          <img src={movieImg} alt={title} />
+          <div className="hover-info">
             <div className="add-icon"></div>
           </div>
         </div>
--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const axiosWithAuth = () => {
   return axios.create({
-   //  baseURL: "https://netflix-clone00.herokuapp.com",
     baseURL: 'https://api.themoviedb.org/3/movie/'
   });
 };
